395a35199cf4f1c6912876c77be037fb8458d7da
Nicholas Szeto <szetonick@gmail.com>
2021-08-01 14:07:39 +0800

    Initial Commit

---
+++ Carpark/CarparkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarparkApp.swift
+//  Carpark
+//
+//  Created by Nicholas Szeto on 1/8/21.
+//
+
+import SwiftUI
+
+@main
+struct CarparkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
